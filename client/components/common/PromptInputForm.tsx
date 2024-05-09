ba634b7e561c31210ed865f8ace3a2ddea9422c2
Andrew Lo <andrewlo957@gmail.com>
2024-05-08 21:34:38 -0700

    create text input bar and button

--- client/components/common/PromptInputForm.tsx
+++ client/components/common/PromptInputForm.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { TextField } from '@radix-ui/themes';
 
 const PromptInputForm = () => {
-  return <div>PromptInputForm Placeholder</div>;
+  return (
+    <form className="mt-48">
+    <TextField.Root placeholder="Generateeeee">
+      <TextField.Slot>
+
+      </TextField.Slot>
+    </TextField.Root>
+    <button className="border-2" type="submit">Generate</button>
+    </form>
+  )
 };
 
 export default PromptInputForm;
