a1e8a70d1650d7777f6f079ea3f8c3f1eb3ba125
zhiyi15 <zhiyihuang1994@gmail.com>
2024-05-09 14:07:04 -0700

    fixed schema issue for LinktaFlow -nodes & edges

--- server/server.ts
+++ server/server.ts
@@ -4,12 +4,13 @@ import genAI from '@server/routes/genAi';
 import { globalErrorHandler } from '@server/middleware/errorHandling';
 import bodyParser from 'body-parser';
 import { MongoClient, ServerApiVersion } from 'mongodb';
+import mongoose from 'mongoose';
 import type { Express, Request, Response } from 'express';
 import type { Server } from 'http';
 
 getEnv();
 const uri = process.env.MONGO_DB_URI;
-
+mongoose.set('strictQuery', false);
 /**
  * Start the server.
  */
@@ -108,6 +109,8 @@ async function connectToDatabase(link: string) {
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
+    await mongoose.connect(uri ?? '')
+    .then(() => console.log('MONGOOSE connected!'))
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
