89b74c4df865daf38db5608ea61064f5d4e76849
zhiyi15 <zhiyihuang1994@gmail.com>
2024-05-10 11:28:19 -0700

    updated treePrompt/COSTAR method with more precise instroctions

--- server/models/TreePromptsModel.ts
+++ server/models/TreePromptsModel.ts
@@ -91,36 +91,35 @@ const TreePrompts = {
   costar(prompt: string): string {
     return `
     # CONTEXT #
-    I am a student looking to learn about a new skill. I am building a tree to
-    organize the information I need to learn. I am looking for a JSON object that
-    I will use to construct this tree.
+    You are an advanced artificial intelligence system, a tool that I can use to help me learn visually. Since I want to digest and learn information quickly, I need your assistance in visualizing various subjects, including paragraphs, full articles, topics, study plans, and more. Your capabilities are not limited to any particular format or type of content.
 
     #######
 
     # Objective #
-    Your task is to build the JSON object that I will use to construct my tree.
-    The first key should be the subject name. The value should be an object with
-    subtopic keys. Each subtopic key should have an array of areas to study.
-
+    First, create a tree-like data structure using nodes, where each node represents a subject. Assign a unique ‘id’ to each node based on its level in the tree, and include a ‘data’ property containing a maximum of three keywords that describe the subject. Here is an example node structure: { id: ‘node1’, data: { label: ‘Software Engineering’ }, children: [ { id: ‘node1.1’, data: { label: ‘Programming Languages’ }, children: [ { id: ‘node1.1.1’, data: { label: ‘Java’ } }, { id: ‘node1.1.2’, data: { label: ‘Python’ } }, { id: ‘node1.1.3’, data: { label: ‘JavaScript’ } } ] }, { id: ‘node1.2’, data: { label: ‘Software Design Patterns’ }, children: [ { id: ‘node1.2.1’, data: { label: ‘Object-Oriented Design’ } }, { id: ‘node1.2.2’, data: { label: ‘Functional Programming’ } }, { id: ‘node1.2.3’, data: { label: ‘Architectural Patterns’ } } ] }, { id: ‘node1.3’, data: { label: ‘Software Testing’ }, children: [ { id: ‘node1.3.1’, data: { label: ‘Unit Testing’ } }, { id: ‘node1.3.2’, data: { label: ‘Integration Testing’ } }, { id: ‘node1.3.3’, data: { label: ‘Performance Testing’ } } ] } ] }
+    Secondly, convert the generated tree-like data structure into React Flow’s nodes and edges array. 
+    Return your response in the exact object format following the example below with no extra information, and Do Not Use Markdown (such as, **Nodes** and **Edges**): 
+    '{"nodes":[{"id":"1","type":"input","data":{"label":"System Design"},"position":{"x":0,"y":0}},{"id":"2","data":{"label":"Principles"},"position":{"x":0,"y":0},"parentId":"1"},{"id":"3","data":{"label":"Patterns"},"position":{"x":0,"y":0},"parentId":"1"},{"id":"4","data":{"label":"Scalability"},"position":{"x":0,"y":0},"parentId":"2"},{"id":"5","data":{"label":"Reliability"},"position":{"x":0,"y":0},"parentId":"2"},{"id":"6","data":{"label":"MVC"},"position":{"x":0,"y":0},"parentId":"3"},{"id":"7","data":{"label":"Microservices"},"position":{"x":0,"y":0},"parentId":"3"}],"edges":[{"id":"e1-2","source":"1","target":"2","type":"smoothstep"},{"id":"e1-3","source":"1","target":"3","type":"smoothstep"},{"id":"e2-4","source":"2","target":"4","type":"smoothstep"},{"id":"e2-5","source":"2","target":"5","type":"smoothstep"},{"id":"e3-6","source":"3","target":"6","type":"smoothstep"},{"id":"e3-7","source":"3","target":"7","type":"smoothstep"}]}'
+    
     #######
 
     # Style #
-    There should be no extra information in the JSON object. Only the structure.
+    A very knowledgable teacher and researcher who can provide neccessary and detailed information to the subjects.
 
     #######
 
     # Tone #
-    By curt and to the point. I am a student looking for information.
+    Friendly.
 
     #######
 
     # Audience #
-    A computer that will parse the JSON object to build a tree.
+    An adult learner.
 
     #######
 
     # Response Format #
-    JSON.
+    Return result in one continues line with no line breaking in JSON format including one object contains two propeties: nodes and edges, which are two arries of React Flow objects; Do Not Use Markdown; 
 
     #######
 
