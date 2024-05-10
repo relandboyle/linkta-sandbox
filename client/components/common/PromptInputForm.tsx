8935309feb5558b747d1ccb019dbdec3fe67077c
Andrew Lo <andrewlo957@gmail.com>
2024-05-10 12:11:14 -0700

    add useMutation

--- client/components/common/PromptInputForm.tsx
+++ client/components/common/PromptInputForm.tsx
@@ -1,15 +1,32 @@
 import { Box, TextField } from '@radix-ui/themes';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import React, { useState } from 'react';
 
 const PromptInputForm = () => {
-  const [promptValue, setPromptValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const newPromptMutation = useMutation({
+    mutationFn: async (userInput) => {
+      const response = await axios.post('http://localhost:3000/gen-ai/query', { input: userInput });
+      return response.data;
+    },
+    onSuccess: (data) => {
+      console.log('Prompt sent successfully', data);
+    },
+    onError: (error) => {
+      console.error('Error sending prompt: ', error);
+    },
+  });
 
   const handleChange = (event) => {
-    setPromptValue(event.target.value);
+    setInputValue(event.target.value);
   };
 
   const handleSubmit = () => {
     // console.log(inputValue);
+    console.log('text typed into input bar: ', inputValue);
+    newPromptMutation.mutate({ input: inputValue });
   };
 
   return (
@@ -17,7 +34,7 @@ const PromptInputForm = () => {
       <Box maxWidth="200px">
         <TextField.Root
           placeholder="Enter your prompt"
-          value={promptValue}
+          value={inputValue}
           onChange={handleChange}
         />
       </Box>
