e0f787fae884ea53e8c1dd0c6fa6cb9e3e16db5d
ChTiSh <chentina.shen@gmail.com>
2024-05-09 21:55:59 -0700

    add useMutation and post request for save tree

--- client/features/output-visualization-page/OutputVisualizationPage.tsx
+++ client/features/output-visualization-page/OutputVisualizationPage.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import TreeVisualizationBox from './components/TreeVisualizationBox';
 import useStore from '@/client/stores/nodeMindMapStore';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 const OutputVisualizationPage = () => {
-  const saveState = () => {
-    const { nodes, edges } = useStore.getState(); // Access Zustand store state directly
-    console.log('Saving state:', { nodes, edges });
-    // Implement the save logic here, e.g., sending to a server or saving to localStorage
-  };
+  const saveFlowStateMutation = useMutation({
+    mutationFn: () => {
+      const { nodes, edges, treeId } = useStore.getState();
+      console.log('saving nodes to tree', nodes);
+      return axios.post(`/api/trees/${treeId}`, { nodes, edges });
+    },
+  });
+
   return (
     <div>
       <h1>Hello World</h1>
       <button
-        onClick={saveState}
-        className="save-button"
+        onClick={() => saveFlowStateMutation.mutate()}
+        className="rounded-md border-2 border-blue-700 bg-blue-300 p-2"
       >
-        Save
+        Save your progress
       </button>
       <TreeVisualizationBox />
     </div>
