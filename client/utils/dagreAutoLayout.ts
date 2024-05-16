a15f9dd22d264878e115890d09ee41c19ea2e1da
ChTiSh <chentina.shen@gmail.com>
2024-05-16 15:41:03 -0700

    switch autolayout from hook to util function as it doesn't involve with react state

--- client/utils/dagreAutoLayout.ts
+++ client/utils/dagreAutoLayout.ts
@@ -1,6 +1,5 @@
-//There are 2 versions on NPM, but only this one is maintained
 import dagre from '@dagrejs/dagre';
-//placeholder for initial nodes and edges types
+// Placeholder for initial nodes and edges types
 import type { Node, Edge } from 'reactflow';
 
 const setupDagreFlow = () => {
@@ -10,9 +9,14 @@ const setupDagreFlow = () => {
   return dagreFlow;
 };
 
-function useAutoLayout(initialNodes: Node[], initialEdges: Edge[], nodeWidth: number, nodeHeight: number) {
+function dagreAutoLayout(
+  initialNodes: Node[],
+  initialEdges: Edge[],
+  nodeWidth: number,
+  nodeHeight: number
+) {
   const dagreGraph = setupDagreFlow();
-  // Return any values or functions that you want to expose to the component using this hook
+
   initialNodes.forEach((node) => {
     dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
   });
@@ -26,23 +30,13 @@ function useAutoLayout(initialNodes: Node[], initialEdges: Edge[], nodeWidth: nu
   initialNodes.forEach((node) => {
     const nodeWithPosition = dagreGraph.node(node.id);
 
-    /*
-     * If you want to use the targetPosition and sourcePosition properties
-     * node.targetPosition = isHorizontal ? 'left' : 'top';
-     * node.sourcePosition = isHorizontal ? 'right' : 'bottom';
-     */
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
     node.position = {
       x: nodeWithPosition.x - nodeWidth / 2,
       y: nodeWithPosition.y - nodeHeight / 2,
     };
-
-    return node;
   });
 
-  return { initialNodes, initialEdges };
+  return { nodes: initialNodes, edges: initialEdges };
 }
 
-export default useAutoLayout;
+export default dagreAutoLayout;
